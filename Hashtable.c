093836b3de0a6d12ce311fe27727f0d9a878a14e
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-27 09:40:04 -0400

    Update Hashtable.c

--- Hashtable.c
+++ Hashtable.c
@@ -8,7 +8,7 @@
 
 #define MAX_BUCKET_SIZE 64
 
-/* Functii de comparare a cheilor */
+/* Key comparison functions */
 int compare_function_ints(void *a, void *b) {
 	int int_a = *((int *)a);
 	int int_b = *((int *)b);
@@ -30,7 +30,7 @@ int compare_function_strings(void *a, void *b) {
 }
 
 
-/* Functii de hashing pentru int-uri si string-uri */
+/* Hashing functions for ints and strings */
 unsigned int hash_function_int(void *a) {
 	/*
 	 * Credits: https://stackoverflow.com/a/12996028/7883884
@@ -57,7 +57,7 @@ unsigned int hash_function_string(void *a) {
 	return hash;
 }
 
-/* Functie care creeaza si initializeaza un hashtable */
+/* Function that creates and initializes a hashtable */
 hashtable_t *ht_create(unsigned int hmax, unsigned int (*hash_function)(void*),
 		               int (*compare_function)(void*, void*)) {
 	/* alocare memorie pentru hashtable */
@@ -66,11 +66,11 @@ hashtable_t *ht_create(unsigned int hmax, unsigned int (*hash_function)(void*),
 
 	ht->hmax = hmax;
 	ht->size = 0;
-    ht->hash_function = hash_function;
-    ht->compare_function = compare_function;
+   	ht->hash_function = hash_function;
+    	ht->compare_function = compare_function;
 
-	/* creeare unui array de liste simplu inlantuite
-	 * si initializarea fiecarei liste in parte 
+	/* create an array of simple chained lists
+	 * and the initialization of each list separately
 	 */
 	ht->buckets = malloc(hmax * sizeof(linked_list_t *));
 	DIE(ht->buckets == NULL, "Allocation Error\n");
@@ -80,11 +80,11 @@ hashtable_t *ht_create(unsigned int hmax, unsigned int (*hash_function)(void*),
 	return ht;
 }
 
-/* Functia care adauga un nou obiect in cadrul unui hashtable */
+/* The function that adds a new object to a hashtable */
 void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
 	        void *value, unsigned int value_size) {
-	/* determinam pozitia in care trebuie sa cautam
-	 * folosind functia de hash
+	/* we determine the position in which we must look
+	 * using the hash function
 	 */
 	unsigned int i = ht->hash_function(key) % ht->hmax;
 	int pos = 0;
@@ -92,8 +92,8 @@ void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
 
 	while (current != NULL) {
 		struct info *info = current->data;
-		/* daca cheia exista deja in hashtable
-		 * doar actualizam valoarea acesteia
+		/* if the key already exists in the hashtable
+		 * we only update its value
 		 */
 		if (ht->compare_function(info->key, key) == 0) {
 			memcpy(info->value, value, value_size);
@@ -102,8 +102,8 @@ void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
 		pos++;
 		current = current->next;
 	}
-	/* daca nu am gasit cheia in hashtable alocam memorie
-	 * pentru obiect, cheie si valore si apoi le adaugam
+	/* if we did not find the key in the hashtable we allocate memory
+	 * for object, key and value and then we add them
 	 */
 	struct info *new_info = malloc(sizeof(struct info));
 	DIE(new_info == NULL, "Allocation Error\n");
@@ -120,15 +120,15 @@ void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
 	free(new_info);
 }
 
-/* Functia care extrage un obiect pe baza unei chei*/
+/* The function that extracts an object based on a key */
 void * ht_get(hashtable_t *ht, void *key) {
 	int i = ht->hash_function(key) % ht->hmax;
-	/* parcurgem lista de pozitia obtinuta cu functia
-	 * de hash si daca gasim un obiect cu cheia dorita
-	 * ii returnam valoarea
-	 */
+	
 	ll_node_t *current = (ht->buckets[i])->head;
-
+	/* scroll through the list of position obtained with the function
+	 * hash and if we find an object with the desired key
+	 * we return the value
+	 */
 	while (current != NULL) {
 		struct info *info = current->data;
 		if (ht->compare_function(info->key, key) == 0) {
@@ -139,14 +139,14 @@ void * ht_get(hashtable_t *ht, void *key) {
 	return NULL;
 }
 
-/* Functia care verifica daca exista un obiect in hastable */
+/* The function that checks if there is an object in the hashtable */
 int ht_has_key(hashtable_t *ht, void *key) {
 	int i = ht->hash_function(key) % ht->hmax;
 	ll_node_t *current = (ht->buckets[i])->head;
 
 	while (current != NULL) {
 		struct info *info = current->data;
-		/* daca gasim obiectul cu cheia dorita returnam 1 */
+		/* if we find the object with the desired key we return 1*/
 			if (ht->compare_function(info->key, key) == 0) {
 			return 1;
 		}
@@ -155,14 +155,14 @@ int ht_has_key(hashtable_t *ht, void *key) {
 	return 0;
 }
 
-/* Functia care elimina un obiect din hastable */
+/* The function that removes an object from the hashtable */
 void ht_remove_entry(hashtable_t *ht, void *key) {
 	int i = ht->hash_function(key) % ht->hmax;
 	ll_node_t *current = (ht->buckets[i])->head;
 	int pos = 0;
-	/* parcurgem lista de pe pozitia obtinuta cu
-	 * funcita de hash iar daca gasim obiectul 
-	 * dorit il stergem si eliberam memoria alocata
+	/* scroll through the list on the position obtained with
+	 * hash function and if we find the object
+	 * if desired, delete it and free the allocated memory
 	 */
 	while (current != NULL) {
 		struct info *info = current->data;
@@ -181,12 +181,12 @@ void ht_remove_entry(hashtable_t *ht, void *key) {
 	}
 }
 
-/* Functia care elibereaza memoria unui hastable */
+/* The function that frees the memory of a hashtable The function that frees the memory of a hasable */
 void ht_free(hashtable_t *ht) {
 	for (unsigned int i = 0; i < ht->hmax; ++i) {
 		ll_node_t *node = ht->buckets[i]->head;
-		/* parcurgem fiecare obiect din cadrul unui
-		 * liste, il stergem, eliberam memoria alocata
+		/* we go through each object within a
+		 * lists, delete it, free up allocated memory
 		 */
 		while (node != NULL) {
 			ll_node_t *prev = node;
@@ -200,14 +200,14 @@ void ht_free(hashtable_t *ht) {
 		}
 		free(ht->buckets[i]);
 	}
-	/* eliberam memoria array-ului de liste inlantuite */
+	/* we free the memory of the array of chained lists */
 	free(ht->buckets);
 	free(ht);
 	ht = NULL;
 }
 
-/* Functia care determina numarul total de noduri 
- * existente in toate bucket-urile
+/* The function that determines the total number of nodes
+ * existing in all buckets
  */
 unsigned int ht_get_size(hashtable_t *ht) {
 	if (ht == NULL) {
@@ -216,7 +216,7 @@ unsigned int ht_get_size(hashtable_t *ht) {
 	return ht->size;
 }
 
-/* Functia care determina numarul total de bucket-uri */
+/* The function that determines the total number of buckets */
 unsigned int ht_get_hmax(hashtable_t *ht) {
 	if (ht == NULL) {
 		return 0;
